32aadd0a7f272503608d315afcf54d97c78e3754
iambodha <165776420+iambodha@users.noreply.github.com>
2024-07-17 22:12:18 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:csv/csv.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,18 +27,80 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final ScrollController _scrollController = ScrollController();
+  Color _backgroundColor = Colors.white;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    final double offset = _scrollController.offset;
+    final double maxScrollExtent = _scrollController.position.maxScrollExtent;
+    final double t = (offset / maxScrollExtent).clamp(0.0, 1.0);
+    setState(() {
+      _backgroundColor = Color.lerp(Colors.white, Colors.white, t)!;
+    });
+  }
+
+  void _showInfoDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('App Information'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('This app is currently in beta.'),
+              SizedBox(height: 10),
+              Text('If you would like to help, please contact:'),
+              Text('somanivibbodh@gmail.com'),
+              SizedBox(height: 10),
+              Text('Developed by: Vibbodh Somani'),
+              Text('Question creation: Bhuvana Reddi and Tia'),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     final sections = [
-      'Theoretical',
-      'Friendship',
-      'Conflict',
-      'Funny',
-      'Viral Questions',
-      'Best Ice Breakers'
+      "Theoretical",
+      "Friendship",
+      "Conflict",
+      "Funny",
+      "Viral Questions",
+      "Best Ice Breakers"
     ];
     final icons = [
       Icons.lightbulb,
@@ -48,155 +111,169 @@ class MyHomePage extends StatelessWidget {
       Icons.ac_unit
     ];
     final sampleQuestions = [
-      'What is the meaning of life?',
-      'How do you define true friendship?',
-      'How do you handle disagreements?',
+      "What is the meaning of life?",
+      "How do you define true friendship?",
+      "How do you handle disagreements?",
       "What's the funniest joke you know?",
-      'If you could be any animal, what would you be?',
+      "If you could be any animal, what would you be?",
       "What's your favorite ice cream flavor?"
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 210,
-        flexibleSpace: ClipRect(
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            heightFactor: 0.7,
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                Image.asset(
-                  'assets/app-bar.png',
-                  fit: BoxFit.cover,
-                  alignment: Alignment.topCenter,
-                ),
-                Container(
-                  alignment: Alignment.bottomLeft,
-                  padding: const EdgeInsets.only(
-                      left: 16.0, top: 16.0, right: 16.0, bottom: 80.0),
-                  child: const Text(
-                    'Ice Breakers',
-                    style: TextStyle(
-                      fontFamily: 'TT-Bluescreens',
-                      color: Colors.white,
-                      fontSize: 40.0,
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 150,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.asset(
+                    'assets/app-bar.png',
+                    fit: BoxFit.cover,
+                    alignment: Alignment.bottomCenter,
+                  ),
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    padding: const EdgeInsets.only(
+                        left: 16.0, right: 16.0, bottom: 80.0),
+                    child: const Text(
+                      'Ice Breakers',
+                      style: TextStyle(
+                        fontFamily: 'TT-Bluescreens',
+                        color: Colors.white,
+                        fontSize: 40.0,
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  alignment: Alignment.bottomRight,
-                  padding: const EdgeInsets.only(
-                      left: 16.0, top: 16.0, right: 16.0, bottom: 80.0),
-                  child: IconButton(
-                    icon: const Icon(Icons.account_circle, color: Colors.white),
-                    onPressed: () {
-                      // Add account button functionality here
-                    },
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    padding: const EdgeInsets.only(
+                        left: 16.0, right: 16.0, bottom: 80.0),
+                    child: IconButton(
+                      icon: const Icon(Icons.info, color: Colors.white),
+                      onPressed: _showInfoDialog,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
           ),
-        ),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: AnimationLimiter(
-        child: ListView.builder(
-          itemCount: sections.length,
-          padding: const EdgeInsets.all(16),
-          itemBuilder: (BuildContext context, int index) {
-            return AnimationConfiguration.staggeredList(
-              position: index,
-              duration: const Duration(milliseconds: 375),
-              child: SlideAnimation(
-                verticalOffset: 50.0,
-                child: FadeInAnimation(
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 16),
-                    child: Hero(
-                      tag: 'section_$index',
-                      child: Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.of(context).push(
-                              PageRouteBuilder(
-                                pageBuilder:
-                                    (context, animation, secondaryAnimation) =>
-                                        QuestionListPage(
-                                  title: sections[index],
-                                  icon: icons[index],
-                                ),
-                                transitionsBuilder: (context, animation,
-                                    secondaryAnimation, child) {
-                                  return FadeTransition(
-                                    opacity: animation,
-                                    child: child,
-                                  );
-                                },
-                              ),
-                            );
-                          },
-                          child: Container(
-                            height: 100,
-                            decoration: BoxDecoration(
-                              color: const Color(0xFF004AAD),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Row(
-                              children: [
-                                const SizedBox(width: 16),
-                                Icon(icons[index],
-                                    size: 48, color: Colors.white),
-                                const SizedBox(width: 16),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        sections[index],
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
+          SliverToBoxAdapter(
+            child: AnimatedBuilder(
+              animation: _scrollController,
+              builder: (context, child) {
+                return Container(
+                  color: _backgroundColor,
+                  child: child,
+                );
+              },
+              child: AnimationLimiter(
+                child: ListView.builder(
+                  itemCount: sections.length,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  itemBuilder: (BuildContext context, int index) {
+                    return AnimationConfiguration.staggeredList(
+                      position: index,
+                      duration: const Duration(milliseconds: 375),
+                      child: SlideAnimation(
+                        verticalOffset: 50.0,
+                        child: FadeInAnimation(
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 16),
+                            child: Hero(
+                              tag: 'section_$index',
+                              child: Material(
+                                color: Colors.transparent,
+                                child: InkWell(
+                                  onTap: () {
+                                    Navigator.of(context).push(
+                                      PageRouteBuilder(
+                                        pageBuilder: (context, animation,
+                                                secondaryAnimation) =>
+                                            QuestionListPage(
+                                          title: sections[index],
+                                          icon: icons[index],
                                         ),
+                                        transitionsBuilder: (context, animation,
+                                            secondaryAnimation, child) {
+                                          return FadeTransition(
+                                            opacity: animation,
+                                            child: child,
+                                          );
+                                        },
                                       ),
-                                      const SizedBox(height: 4),
-                                      Text(
-                                        sampleQuestions[index],
-                                        style: const TextStyle(
-                                          color: Colors.white70,
-                                          fontSize: 14,
+                                    );
+                                  },
+                                  child: Container(
+                                    height: 100,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xFF004AAD),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        const SizedBox(width: 16),
+                                        Icon(icons[index],
+                                            size: 48, color: Colors.white),
+                                        const SizedBox(width: 16),
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                sections[index],
+                                                style: const TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              const SizedBox(height: 4),
+                                              Text(
+                                                sampleQuestions[index],
+                                                style: const TextStyle(
+                                                  color: Colors.white70,
+                                                  fontSize: 14,
+                                                ),
+                                                maxLines: 2,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ],
+                                          ),
                                         ),
-                                        maxLines: 2,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                    ],
+                                        const SizedBox(width: 16),
+                                      ],
+                                    ),
                                   ),
                                 ),
-                                const SizedBox(width: 16),
-                              ],
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
+                    );
+                  },
                 ),
               ),
-            );
-          },
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
-class QuestionListPage extends StatelessWidget {
+class QuestionListPage extends StatefulWidget {
   final String title;
   final IconData icon;
 
@@ -204,10 +281,35 @@ class QuestionListPage extends StatelessWidget {
       : super(key: key);
 
   @override
+  _QuestionListPageState createState() => _QuestionListPageState();
+}
+
+class _QuestionListPageState extends State<QuestionListPage> {
+  List<Map<String, dynamic>> questions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadQuestions();
+  }
+
+  Future<void> _loadQuestions() async {
+    final String csvString = await DefaultAssetBundle.of(context)
+        .loadString('assets/${widget.title.toLowerCase()}_questions.csv');
+    List<List<dynamic>> csvTable = CsvToListConverter().convert(csvString);
+
+    setState(() {
+      questions = csvTable
+          .map((row) => {'question': row[0], 'completed': false})
+          .toList();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
         backgroundColor: const Color(0xFF004AAD),
         foregroundColor: Colors.white,
       ),
@@ -217,7 +319,7 @@ class QuestionListPage extends StatelessWidget {
             padding: const EdgeInsets.all(16),
             child: TextField(
               decoration: InputDecoration(
-                hintText: 'Search questions...',
+                hintText: "Search questions...",
                 prefixIcon: Icon(Icons.search),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
@@ -228,7 +330,7 @@ class QuestionListPage extends StatelessWidget {
           Expanded(
             child: AnimationLimiter(
               child: ListView.builder(
-                itemCount: 20, // Replace with actual question count
+                itemCount: questions.length,
                 itemBuilder: (BuildContext context, int index) {
                   return AnimationConfiguration.staggeredList(
                     position: index,
@@ -236,11 +338,13 @@ class QuestionListPage extends StatelessWidget {
                     child: SlideAnimation(
                       verticalOffset: 50.0,
                       child: FadeInAnimation(
-                        child: ListTile(
-                          leading: Icon(icon),
-                          title: Text('Question ${index + 1}'),
-                          onTap: () {
-                            // Handle question tap
+                        child: CheckboxListTile(
+                          title: Text(questions[index]['question']),
+                          value: questions[index]['completed'],
+                          onChanged: (bool? value) {
+                            setState(() {
+                              questions[index]['completed'] = value;
+                            });
                           },
                         ),
                       ),
